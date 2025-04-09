a5a3759989746426fc966a3246621cb28fef1b0e
Shanilka Srimal <sanilkasrimal01@gmail.com>
2025-04-09 22:27:47 +0530

    fix some errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import UserDashboard from "./pages/UserDashboard"; 
-import MonetizationForm from "./pages/monetizationAndRewardManagement/monetizationRequestForm";
+import UserDashboard from "./pages/UserDashboard";
+import Posts from './pages/Posts/Posts';
+import PostDetail from './pages/Posts/PostDetail';
+import CreatePost from "./pages/Posts/CreatePost";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Core Pages */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/userdashboard" element={<UserDashboard />} /> 
-
-        {/*monetization and reward managemrnt*/}
-        <Route path="/monetizationform" element={<MonetizationForm/>} />
         
+        {/* Post Management */}
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/add-post" element={<CreatePost />} />
+        
+        {/* User Management */}
+        <Route path="/userdashboard" element={<UserDashboard />} />
+        
+        {/* Optional: Category Filter */}
+        <Route path="/category/:categoryName" element={<Home />} />
       </Routes>
     </Router>
   );
